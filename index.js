af18322a3db351e11eb9918c9c3502202ae8b835
albertusro1 <albertusrowan@gmail.com>
2026-06-18 23:40:21 +0700

    Force stable webVersionCache to fix couldn't link device

--- index.js
+++ index.js
@@ -61,6 +61,10 @@ async function logUserExpense(userName, amount, description, category, customDat
 
 const client = new Client({
     authStrategy: new LocalAuth({ dataPath: '/.wwebjs_auth' }),
+    webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
+    },
     puppeteer: {
         executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
         timeout: 0,
